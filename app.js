2564aa812688848726202f86a304fbcdd7d6a743
hwamoc <hwamoc@hwamocs-MacBook-Pro.local>
2018-12-12 02:44:35 +0900

    contest edit error fixed

--- app.js
+++ app.js
@@ -95,11 +95,11 @@ app.use('/questions', questions);
 require('./routes/auth')(app, passport);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
 // error handler
 app.use(function(err, req, res, next) {
--- routes/contests.js
+++ routes/contests.js
@@ -15,6 +15,62 @@ function needAuth(req, res, next) {
   }
 }
 
+function validateForm(form, options) {
+  var title = form.title || "";
+  var author = user._id || "";
+  var host = form.host || "";
+  var field = form.field || "";
+  var applicant = form.applicant || "";
+  var startDate = form.startDate || "";
+  var endDate = form.endDate || "";
+  var personInCharge = form.personInCharge || "";
+  var contact = form.contact || "";
+  var prize = form.prize || "";
+  var content = form.content || "";
+  var tags = form.tags.split(" ").map(e => e.trim()) || "";
+
+  title = title.trim();
+  content = content.trim();
+  // event_description = event_description.trim();
+  // organizer = organizer.trim();
+  // organizer_description = organizer_description.trim();
+
+  if (!title) {
+    return 'Title is required.';
+  }
+  if (!author) {
+    return 'Location is required.';
+  }
+  if (!host) {
+    return 'Start date is required.';
+  }
+  if(!field){
+    return 'Start time is required.';
+  }
+  if(!applicant) {
+    return 'Start time is required.';
+  }
+  if (!startDate) {
+    return 'End date is required.';
+  }
+  if(!endDate) {
+    return 'End time is required.';
+  }
+  if(!personInCharge) {
+    return 'End time is required.';
+  }
+  if (!contact) {
+    return 'Event description is required.';
+  }
+  if (!prize) {
+    return 'Organizer is required.';
+  }
+  if (!content) {
+    return 'Organizer description is required.';
+  }
+  return null;
+}
+
 /* GET contests listing. */
 router.get('/', catchErrors(async (req, res, next) => {
   const page = parseInt(req.query.page) || 1;
@@ -62,6 +118,15 @@ router.put('/:id', catchErrors(async (req, res, next) => {
     return res.redirect('back');
   }
   contest.title = req.body.title;
+  contest.author = user._id;
+  contest.host = req.body.host;
+  contest.field = req.body.field;
+  contest.applicant = req.body.applicant;
+  contest.startDate = req.body.startDate;
+  contest.endDate = req.body.endDate;
+  contest.personInCharge = req.body.personInCharge;
+  contest.contact = req.body.contact;
+  contest.prize = req.body.prize;
   contest.content = req.body.content;
   contest.tags = req.body.tags.split(" ").map(e => e.trim());
 
@@ -84,8 +149,8 @@ router.post('/', needAuth, catchErrors(async (req, res, next) => {
     host: req.body.host,
     field: req.body.field,
     applicant: req.body.applicant,
-    startDate: new Date(req.body.startDate),
-    endDate: new Date(req.body.endDate),
+    startDate: req.body.startDate,
+    endDate: req.body.endDate,
     personInCharge: req.body.personInCharge,
     contact: req.body.contact,
     prize: req.body.prize,
@@ -94,6 +159,27 @@ router.post('/', needAuth, catchErrors(async (req, res, next) => {
   });
   await contest.save();
   req.flash('success', 'Successfully posted');
+  res.redirect('/contests');
+}));
+
+router.post('/:id', needAuth, catchErrors(async (req, res, next) => {
+  const user = req.user;
+  var contest = new Contest({
+    title: req.body.title,
+    author: user._id,
+    host: req.body.host,
+    field: req.body.field,
+    applicant: req.body.applicant,
+    startDate: req.body.startDate,
+    endDate: req.body.endDate,
+    personInCharge: req.body.personInCharge,
+    contact: req.body.contact,
+    prize: req.body.prize,
+    content: req.body.content,
+    tags: req.body.tags.split(" ").map(e => e.trim()),
+  });
+  await contest.save();
+  req.flash('success', 'Successfully updated');
   res.redirect('/contests');
 }));
 
